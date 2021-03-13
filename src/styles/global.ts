67b8715283ca2ec504ad68d607a42d7787028e17
Bruno Tomé <ibrunotome@gmail.com>
2021-03-13 19:49:25 -0300

    feat: static page 'About'

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,10 @@ const GlobalStyles = createGlobalStyle`
     --highlight: #e20e8d;
     --background: #030518;
     --white: #eeeeee;
+    --grey: #cccccc;
+
     --container: 100rem;
+
     --small: 1.5rem;
     --medium: 3rem;
     --large: 5rem;
@@ -24,20 +27,21 @@ const GlobalStyles = createGlobalStyle`
   html, body, #__next {
     height: 100%;
     background: var(--background);
-    color: var(--white)
+    color: var(--white);
   }
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
   }
 
-  p {
+  p,
+  a {
     font-size: 2rem;
-    line-height: var(--medium)
+    line-height: var(--medium);
   }
 
-  p, a {
-    color: var(--highlight)
+  a {
+    color: var(--highlight);
   }
 `
 
---
+++ src/templates/About/index.tsx
@@ -0,0 +1,25 @@
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+import LinkWrapper from 'components/LinkWrapper'
+
+import * as S from './styles'
+
+const AboutTemplate = () => (
+  <S.Content>
+    <LinkWrapper href="/">
+      <CloseOutline size={32} />
+    </LinkWrapper>
+
+    <S.Heading>MY Trips</S.Heading>
+
+    <S.Body>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus numquam
+        veniam, amet aliquam optio labore officia, nobis ipsum hic eligendi
+        voluptas incidunt unde asperiores! Dolorum natus corrupti veritatis a
+        quidem.
+      </p>
+    </S.Body>
+  </S.Content>
+)
+
+export default AboutTemplate
