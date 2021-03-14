212c78aa76bd0f7648bbd158074898728b5c8b86
Bruno Tomé <ibrunotome@gmail.com>
2021-03-14 09:25:42 -0300

    feat: dynamic pages with getStaticProps and getStaticPages

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -3,21 +3,21 @@ import LinkWrapper from 'components/LinkWrapper'
 
 import * as S from './styles'
 
-const PageTemplate = () => (
+export type PageTemplateProps = {
+  heading: string
+  body: string
+}
+
+const PageTemplate = ({ heading, body }: PageTemplateProps) => (
   <S.Content>
     <LinkWrapper href="/">
       <CloseOutline size={32} />
     </LinkWrapper>
 
-    <S.Heading>MY Trips</S.Heading>
+    <S.Heading>{heading}</S.Heading>
 
     <S.Body>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus numquam
-        veniam, amet aliquam optio labore officia, nobis ipsum hic eligendi
-        voluptas incidunt unde asperiores! Dolorum natus corrupti veritatis a
-        quidem.
-      </p>
+      <div dangerouslySetInnerHTML={{ __html: body }} />
     </S.Body>
   </S.Content>
 )
